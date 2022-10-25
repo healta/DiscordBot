91124c5591d548f8a1506bea86fec1bb694c64a8
healta <dimitar.vasilev.dimitrov95@gmail.com>
2022-10-25 09:10:45 +0300

    checking for guids works now

--- discordBot.py
+++ discordBot.py
@@ -3,15 +3,17 @@ import discord
 import json
 
 #Discord token can be acquired from the discord app as just a regular user.
-TOKEN = ""
+TOKEN = "OTg5NDYyNzAxMjkyMDE1NjE2.GPhTzM.8tK2Yry0WSdLg84BqI3FpfoJkPfVFFQBl38lOs"
 
 intents = discord.Intents.default()
+
 intents.message_content = True
+
 client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print("We have loggen in as {0.user}".format(client))
+    print("We have loggen in as {}".format(client))
 
 @client.event
 async def on_message(message):
@@ -21,33 +23,36 @@ async def on_message(message):
 #checks if player is active and reports back in chat, to be active message needs to start with "!"
     elif message.content.startswith('!'):
         cmd = message.content.split()[0].replace("!", "")
-        link = 'https://armory.warmane.com/api/character/random/Icecrown/summary'
-        properlink = link.replace("random", cmd)
-        apidata = requests.get(properlink, headers={'User-Agent': 'Custom'})
-        disc = (apidata.text)
-        jdata = json.loads(disc)
+        link = 'https://armory.warmane.com/api/character/{}/Icecrown/summary'.format(cmd)
+        apidata = requests.get(link, headers={'User-Agent': 'Custom'})
+
+        jdata = json.loads(apidata.text)
 
         if jdata.get("online") is False:
             await message.channel.send(cmd + " is offline :(")
         else:
             await message.channel.send(cmd+ " is online :)")
-        if len(message.content.split()) > 1:
-            parameters = message.content.split()[1:]
 
 #checks if guild is active and reports back in chat, to be active message needs to start with "#"        
     elif message.content.startswith('#'):
         guildrequest = message.content.split()[0].replace("#", "")
-        guildweb = "http://armory.warmane.com/api/guild/random/Icecrown/summary"
-        guildwebproper = guildweb.replace("random", guildrequest)
-        apidata = requests.get(guildwebproper, headers={'User-Agent': 'Custom'})
-        disc = apidata.text
-        jdata = json.loads(disc)
+        guildweb = "http://armory.warmane.com/api/guild/{}/Icecrown/summary".format(guildrequest)
+        apidata = requests.get(guildweb, headers={'User-Agent': 'Custom'})
 
-        for elementos in jdata.keys():
-            print (elementos)
-            for elementos2 in jdata[elementos].keys():
-                print(elementos2)
+        jdata = json.loads(apidata.text)
 
-        await message.channel.send(results)
+        results = []
 
+        counter= 0
+
+        await message.channel.send("Checking online players for guild {}".format(guildrequest))
+
+        for i in jdata["roster"]:
+            if i["online"]==True:
+                await message.channel.send(i["name"] + " is online :)")
+                counter+=1
+
+        await message.channel.send("There are {} online players from {}".format(counter,guildrequest))
+        
+        
 client.run(TOKEN)
